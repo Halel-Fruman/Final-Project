a77ec30c4b8cf8e2f49a8967003d7583d18dbd34
Halel-Fruman <Halel900@gmail.com>
2025-01-13 18:33:26 +0200

    merged and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import HomePage from './pages/HomePage/HomePage.js';
-import ProductPage from './pages/ProductPage/ProductPage.js';
-import LoginPage from './pages/PersonalArea/LoginPage.jsx';
-import PersonalArea from './pages/PersonalArea/PersonalArea.jsx';
-import CartPage from './pages/PersonalArea/CartPage.js';
-import Header from './components/Header/Header.js';
-import Footer from './components/Footer/Footer.js';
-import SysAdmin from './pages/SysAdmin/SysAdmin.jsx';
-// import StoreManagment from './pages/StoreManagment/StoreManagment.jsx';
-import RegisterPage from './pages/Registeration/RegisterPage.jsx';
-import './App.css';
-import '@fontsource/rubik';
 
-const App = () => {
-  const { i18n } = useTranslation();
-  const [token, setToken] = useState(localStorage.getItem('token'));
-  const [userId, setUserId] = useState(localStorage.getItem('userId'));
-
-  useEffect(() => {
-    // Set direction (LTR or RTL) based on selected language
-    document.documentElement.dir = i18n.language === 'he' ? 'rtl' : 'ltr';
-  }, [i18n.language]);
-
-  const handleLogout = () => {
-    setToken(null);
-    setUserId(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('userId');
-  };
-
-  return (
-    <Router>
-      <Header onLogout={handleLogout} isLoggedIn={!!token} />
-      <div className="app-content">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/login" element={token ? <Navigate to="/personal-area" /> : <LoginPage setToken={setToken} setUserId={setUserId} />} />
-          <Route path="/personal-area" element={token ? <PersonalArea userId={userId} /> : <Navigate to="/login" />} />
-          <Route path="/cart" element={token ? <CartPage userId={userId} /> : <Navigate to="/login" />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* <Route path='/loginAdmin' element={token ? <Navigate to="/AdminPage" /> : <LoginPage setToken={setToken} setAdminId={setAdminId} />} /> */}
-          {/* <Route path='/SysAdmin' element={token ? <SysAdmin AdminId={AdminId} /> : <Navigate to="/loginAdmin" />} /> */}
-          <Route path="/SysAdmin" element={<SysAdmin />} />
-
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  );
-};
-
-export default App;
 import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
@@ -69,6 +14,7 @@ import PersonalArea from "./pages/PersonalArea/PersonalArea.jsx";
 import CartPage from "./pages/PersonalArea/CartPage.js";
 import RegisterPage from "./pages/Registeration/RegisterPage.jsx";
 import AddAddressPage from "./pages/Registeration/AddAddressPage.jsx";
+import SysAdmin from './pages/SysAdmin/SysAdmin.jsx';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
@@ -180,6 +126,8 @@ const App = () => {
             path="/register"
             element={<RegisterPage setToken={setToken} setUserId={setUserId} />}
           />
+                    <Route path="/SysAdmin" element={<SysAdmin />} />
+
           <Route
             path="/personal-area"
             element={
